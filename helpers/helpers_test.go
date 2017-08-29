f7415cc2bb2decfa3c7e52bf8a4e621b38c11e62
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2017-08-29 13:22:12 +0300

    Add test helpers.

---
+++ helpers/helpers_test.go
@@ -0,0 +1,69 @@
+// Copyright 2017 Percona LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helpers
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
+)
+
+func TestReadMetric(t *testing.T) {
+	for expected, m := range map[*Metric]prometheus.Metric{
+		&Metric{
+			"counter",
+			prometheus.Labels{"job": "test1", "instance": "test2"},
+			dto.MetricType_COUNTER,
+			36.6,
+		}: prometheus.MustNewConstMetric(
+			prometheus.NewDesc("counter", "unused #HELP text", []string{"instance"}, prometheus.Labels{"job": "test1"}),
+			prometheus.CounterValue,
+			36.6,
+			"test2",
+		),
+
+		&Metric{
+			"gauge",
+			prometheus.Labels{"job": "test1", "instance": "test2"},
+			dto.MetricType_GAUGE,
+			36.6,
+		}: prometheus.MustNewConstMetric(
+			prometheus.NewDesc("gauge", "unused #HELP text", []string{"instance"}, prometheus.Labels{"job": "test1"}),
+			prometheus.GaugeValue,
+			36.6,
+			"test2",
+		),
+
+		&Metric{
+			"untyped",
+			prometheus.Labels{"job": "test1", "instance": "test2"},
+			dto.MetricType_UNTYPED,
+			36.6,
+		}: prometheus.MustNewConstMetric(
+			prometheus.NewDesc("untyped", "unused #HELP text", []string{"instance"}, prometheus.Labels{"job": "test1"}),
+			prometheus.UntypedValue,
+			36.6,
+			"test2",
+		),
+	} {
+		actual := ReadMetric(m)
+		if !reflect.DeepEqual(expected, actual) {
+			t.Errorf("expected = %+v\nactual = %+v", expected, actual)
+		}
+
+	}
+}
