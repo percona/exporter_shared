cbaae0cfa2ad1b00314a58e9d4f431bf10a97cda
Carlos Salguero <carlos.salguero@percona.com>
2021-05-20 10:17:42 -0300

    Revert "Revert "Merge pull request #15 from percona/PMM-7789_remove_insecure_cyphers""
    This reverts commit b023149b6892e6025c5b7ef6717487a2b3c2bbad.

--- server.go
+++ server.go
@@ -99,16 +99,13 @@ func TLSConfig() *tls.Config {
 		MinVersion:               tls.VersionTLS12,
 		PreferServerCipherSuites: true,
 		CipherSuites: []uint16{
-			// no SHA-1, ECDHE before plain RSA, GCM before CBC
+			// no SHA-1, no CBC, ECDHE before plain RSA
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256,
 			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
 			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_RSA_WITH_AES_128_CBC_SHA256,
 		},
 	}
 }
